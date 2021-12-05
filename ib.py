605c596f83959c5a1edbe204ddc3be1ff87dc1a6
Kashifrazabb <45673871+Kashifrazabb@users.noreply.github.com>
2021-12-05 21:06:14 +0500

    Update ib.py

--- ib.py
+++ ib.py
@@ -4,7 +4,7 @@ bot = Bot()
 
 bot.login(username="cohoxe9464",password="KashiJee786143")
 
-followers = bot.get_user_followers("affirmationsandquotes",100)
+followers = bot.get_user_followers("affirmationsandquotes")
 
 with open('followers.txt', 'a') as file:
     file.write('\n'.join(followers) + "\n")
