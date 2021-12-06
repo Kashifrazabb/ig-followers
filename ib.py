61493daccf0f32a1e0390f625e68a0d047284447
Kashifrazabb <45673871+Kashifrazabb@users.noreply.github.com>
2021-12-06 21:55:20 +0500

    Update ib.py

--- ib.py
+++ ib.py
@@ -7,5 +7,5 @@ bot.login(username="cohoxe9464",password="KashiJee786143")
 followers = bot.get_user_followers("affirmationsandquotes")
 
 for id in followers:
-    username=bot.get_username_from_id(id)
+    username=bot.get_username_from_user_id(id)
     print(username)
