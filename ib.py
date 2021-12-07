1b00db6781e82f269ffb07dbf27270b111700dde
Kashifrazabb <45673871+Kashifrazabb@users.noreply.github.com>
2021-12-07 21:11:38 +0500

    Update ib.py

--- ib.py
+++ ib.py
@@ -4,9 +4,10 @@ bot = Bot()
 
 bot.login(username="xeyov61622",password="I have12")
 
-followers = bot.get_user_followers("poetalia1")
+followers = bot.get_user_followers("affirmationsandquotes")
 
 with open("get_followers.txt", "a") as f:
     for i in followers:
-        print(i)
-        f.write(str(bot.get_username_from_user_id(i)) +"\n")
+        username = bot.get_username_from_user_id(i)
+        f.write(str(username) +"\n")
+        print(username)
